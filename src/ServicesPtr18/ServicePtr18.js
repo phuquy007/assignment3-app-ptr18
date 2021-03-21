eaf0b68315ab4097a0e992c284fb748d394fbd49
Ryan Tran <tranp6@student.douglascollege.ca>
2021-03-21 00:21:36 -0700

    Added addItem to WS

--- src/ServicesPtr18/ServicePtr18.js
+++ src/ServicesPtr18/ServicePtr18.js
@@ -3,9 +3,30 @@ const API_URL = "http://localhost:8000/";
 export async function getInventory(){
     return fetch(`${API_URL}inventoryPtr18`,
     {
-        methods: 'GET',
+        method: 'GET',
         headers: { 'Content-Type': 'application/json' }
         
+    })
+    .then(response => response.json());
+}
+
+export async function getCart(){
+    return fetch(`${API_URL}cartPtr18`,
+    {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+        
+    })
+    .then(response => response.json());
+}
+
+export async function addItemToCart_WS(sku){
+    const data = JSON.stringify(sku);
+    return fetch(`${API_URL}cartPtr18`,
+    {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: data
     })
     .then(response => response.json());
 }
